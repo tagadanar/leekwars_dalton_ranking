47d7eb08d04f8d447877a2e8b41ca18bcfe527c1
tagadanar <raphael.renon@gmail.com>
2026-02-23 19:34:13 +0100

    Use luckyleek.png as team section avatar

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const DALTON_HATS = {
 };
 
 const FARMER_AVATAR = "https://leekwars.com/avatar/42851.png";
-const TEAM_EMBLEM = "https://leekwars.com/image/emblem/10151.png";
+const TEAM_EMBLEM = "luckyleek.png";
 
 // Star medals for top 3 (gold / silver / bronze)
 const STAR = '<path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 22 12 18.27 5.82 22 7 14.14l-5-4.87 6.91-1.01z"/>';
